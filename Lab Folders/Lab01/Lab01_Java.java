bf17f65fe0290f21a80a6335c5212ddb8d7a8003
Umer Mohammed <D3m0nspider@gmail.com>
2024-02-02 02:47:47 +0000

    Edited Lab01_Java in VSCode

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
   //Edit made by UmerMohammed_220181356
+  //Edit made by UmerMohammed_220181356 in VSCODE. Chose vscode over gitpod due to previous usage and wanting to further experience using vscode.
 
   //Last line print('Good bye')
 }
